8b7f590e1feb3c63f1463e2680ab95a7535607ff
1301070 <c.m.w.carroll.rgu.ac.uk>
2015-10-19 15:39:48 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        </head>
+        <body>
+            <p>
+                <?php
+                    echo "Hello world";
+                ?>
+            </p>
+        </body>
+
+</html>
+/**
+ * Created by PhpStorm.
+ * User: 1301070
+ * Date: 19/10/2015
+ * Time: 15:32
+ */
