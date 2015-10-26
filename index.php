efaea804f6ac3d980a102e68406bcd78daca3fd7
1301070 <c.m.w.carroll.rgu.ac.uk>
2015-10-26 14:11:43 +0000

    first commit

--- index.php
+++ index.php
@@ -5,20 +5,10 @@
         <body>
             <p>
                 <?php
-                    $numberOfHobbits = 2;
-
-                    switch ($numberOfHobbits){
-                        case 1:
-                            echo "1 sad hobbit";
-                            break;
-                        case 2:
-                            echo "2 happy hobbits";
-                            break;
-                        case 3:
-                            echo "3 hobbits are a crowd";
-                            break;
-                        default:
-                            echo "All the hobbits have gone home";
+                    $query = $ GET["query"];
+                    $type = $GET["type"];
+                    echo "The query is " . $cat . " and the type is " . $type";
+                ?>
                     }
                 ?>
             </p>
